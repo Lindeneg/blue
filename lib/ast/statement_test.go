8bfe6dee526ef76e0da13f093e9aa76ef4851d64
lindeneg <christian@lindeneg.org>
2024-11-18 20:19:17 +0100

    ast statement tests

---
+++ lib/ast/statement_test.go
@@ -0,0 +1,73 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/lindeneg/blue/lib/token"
+)
+
+func TestStatements(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.T{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.T{Type: token.IDENTIFIER, Literal: "foo"},
+					Value: "foo",
+				},
+				Value: &Identifier{
+					Token: token.T{Type: token.IDENTIFIER, Literal: "bar"},
+					Value: "bar",
+				},
+			},
+			&ConstStatement{
+				Token: token.T{Type: token.CONST, Literal: "const"},
+				Name: &Identifier{
+					Token: token.T{Type: token.IDENTIFIER, Literal: "foo"},
+					Value: "foo",
+				},
+				Value: &Identifier{
+					Token: token.T{Type: token.IDENTIFIER, Literal: "bar"},
+					Value: "bar",
+				},
+			},
+			&BlockStatement{
+				Token: token.T{Type: token.LBRACE, Literal: "{"},
+				Statements: []Statement{
+					&ConstStatement{
+						Token: token.T{Type: token.CONST, Literal: "const"},
+						Name: &Identifier{
+							Token: token.T{Type: token.IDENTIFIER, Literal: "foo"},
+							Value: "foo",
+						},
+						Value: &Identifier{
+							Token: token.T{Type: token.IDENTIFIER, Literal: "bar"},
+							Value: "bar",
+						},
+					},
+				},
+			},
+			&ReturnStatement{
+				Token: token.T{Type: token.RETURN, Literal: "return"},
+				ReturnValue: &Identifier{
+					Token: token.T{Type: token.IDENTIFIER, Literal: "foo"},
+					Value: "foo",
+				},
+			},
+			&ExpressionStatement{
+				Token: token.T{Type: token.IDENTIFIER, Literal: "foo"},
+				Expression: &Identifier{
+					Token: token.T{Type: token.IDENTIFIER, Literal: "foo"},
+					Value: "foo",
+				},
+			},
+		},
+	}
+	expected := "let foo = bar;const foo = bar;" +
+		"{const foo = bar;}" +
+		"return foo;foo"
+
+	if program.String() != expected {
+		t.Errorf("program.String() wrong. got=%q, want=%q", program.String(), expected)
+	}
+}
