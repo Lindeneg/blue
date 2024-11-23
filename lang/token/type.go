5909fe442db4a243f962efc28a098aa1e2a72cb2
lindeneg <christian@lindeneg.org>
2024-11-23 03:53:57 +0100

    token name

--- lang/token/type.go
+++ lang/token/type.go
@@ -3,6 +3,10 @@ package token
 // Type is all tokens currently understood
 type Type int
 
+func (t Type) String() string {
+	return name[t]
+}
+
 const (
 	UNKNOWN    Type = iota // unknown token
 	EOF                    // end of file
@@ -45,3 +49,46 @@ const (
 	FOR                    // for keyword
 	NULL                   // null keyword
 )
+
+var name = map[Type]string{
+	UNKNOWN:    "UNKNOWN",
+	EOF:        "EOF",
+	IDENTIFIER: "IDENTIFIER",
+	INT:        "INT",
+	FLOAT:      "FLOAT",
+	STRING:     "STRING",
+	ASSIGN:     "=",
+	PLUS:       "+",
+	MINUS:      "-",
+	BANG:       "!",
+	STAR:       "*",
+	FSLASH:     "/",
+	COLON:      ":",
+	SCOLON:     ";",
+	COMMA:      ",",
+	LT:         "<",
+	GT:         ">",
+	EQ:         "==",
+	NEQ:        "!=",
+	LTOE:       "<=",
+	GTOE:       ">=",
+	AND:        "&&",
+	OR:         "||",
+	LPAREN:     "(",
+	RPAREN:     ")",
+	LBRACE:     "{",
+	RBRACE:     "}",
+	LBRACKET:   "[",
+	RBRACKET:   "]",
+	DEF:        "DEF",
+	LET:        "LET",
+	CONST:      "CONST",
+	TRUE:       "TRUE",
+	FALSE:      "FALSE",
+	IF:         "IF",
+	ELIF:       "ELIF",
+	ELSE:       "ELSE",
+	RETURN:     "RETURN",
+	FOR:        "FOR",
+	NULL:       "NULL",
+}
